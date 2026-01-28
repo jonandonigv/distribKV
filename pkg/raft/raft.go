c2918755d9e4bd35cd010b53593fa70ceeab5543
jonandonigv <jonandonigv@gmail.com>
2026-01-28 15:31:33 +0100

    wip: Raft implementation

--- pkg/raft/raft.go
+++ pkg/raft/raft.go
@@ -1,3 +1,27 @@
 package raft
 
-type Raft struct{}
+import (
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+type Raft struct {
+	mu    sync.Mutex
+	peers []*grpc.ClientConn // Subject to change
+	me    int
+
+	// Persistent state
+	currentTerm int
+	votedFor    int
+	log         []LogEntry
+
+	// Volatile state
+	state       State
+	commitIndex int
+	lastApplied int
+
+	// Leader state
+	nextIndex  []int
+	matchIndex []int
+}
