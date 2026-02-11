2d742808e07c4027dbf83a6918cce2209afd5552
jonandonigv <jonandonigv@gmail.com>
2026-02-11 19:36:35 +0100

    wip: KVserver types

---
+++ pkg/kvserver/types.go
@@ -0,0 +1,66 @@
+package kvserver
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/jonandonigv/distribKV/pkg/raft"
+	pb "github.com/jonandonigv/distribKV/proto/kv"
+	"google.golang.org/grpc"
+)
+
+var (
+	ErrNotLeader   = errors.New("not leader")
+	ErrTimeout     = errors.New("timeout waiting for commit")
+	ErrKeyNotFound = errors.New("key not found")
+	ErrDuplicate   = errors.New("duplicate operation")
+)
+
+type OpType int
+
+const (
+	OpGet OpType = iota
+	OpPut
+	OpAppend
+)
+
+type Op struct {
+	Type       OpType
+	Key        string
+	Value      string
+	ClientId   int64
+	SequenceId int64
+}
+
+type Result struct {
+	Value string
+	Err   error
+}
+
+type PendingOp struct {
+	Index    int
+	Op       Op
+	ResultCh chan Result
+}
+
+type KVServer struct {
+	mu         sync.Mutex
+	rf         *raft.Raft
+	applyCh    chan raft.ApplyMsg
+	state      map[string]string
+	duplicates map[int64]map[int64]Result
+	pendingOps map[int]*PendingOp
+	leaderId   int
+	dead       bool
+	shutdownCh chan struct{}
+}
+
+type Clerk struct {
+	servers  []string
+	leaderId int
+	clientId int64
+	seqNum   int64
+	mu       sync.Mutex
+	conns    map[int]*grpc.ClientConn
+	clients  map[int]pb.KVClient
+}
